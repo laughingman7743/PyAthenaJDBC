be6dd55466fa8a3ad31dd168636bf31605f9d058
laughingman7743 <laughingman7743@gmail.com>
2017-04-02 18:31:17 +0900

    Register dialect for testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from future.utils import PY26
-
+from sqlalchemy.dialects import registry
 
 if PY26:
     import unittest2 as unittest
 else:
     import unittest  # noqa: F401
+
+
+registry.register("awsathena.jdbc", "pyathenajdbc.sqlalchemy_athena", "AthenaDialect")
