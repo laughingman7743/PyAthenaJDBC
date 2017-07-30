18d02a74255521abb903368116a9959d3bef1e80
laughingman7743 <laughingman7743@gmail.com>
2017-07-30 11:19:48 +0900

    Add test for query execution information

---
+++ tests/test_util.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from datetime import datetime
+
+from pyathenajdbc.util import to_datetime
+
+from tests import unittest
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_to_datetime(self):
+        self.assertIsNone(to_datetime(None))
+        import jpype
+        cal = jpype.java.util.Calendar.getInstance()
+        cal.set(jpype.java.util.Calendar.YEAR, 2017)
+        cal.set(jpype.java.util.Calendar.MONTH, 0)
+        cal.set(jpype.java.util.Calendar.DAY_OF_MONTH, 1)
+        cal.set(jpype.java.util.Calendar.HOUR_OF_DAY, 2)
+        cal.set(jpype.java.util.Calendar.MINUTE, 3)
+        cal.set(jpype.java.util.Calendar.SECOND, 4)
+        java_date = cal.getTime()
+        py_datetime = to_datetime(java_date)
+        self.assertEqual(py_datetime, datetime(2017, 1, 1, 2, 3, 4))
