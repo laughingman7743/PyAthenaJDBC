6db64b78000d97c1de666a56ac89272581250eae
laughingman7743 <laughingman7743@gmail.com>
2020-08-02 23:06:01 +0900

    Bump version to 2.1.0

--- pyathenajdbc/__init__.py
+++ pyathenajdbc/__init__.py
@@ -5,7 +5,7 @@ import datetime
 
 from pyathenajdbc.error import *  # noqa
 
-__version__ = "2.0.9"
+__version__ = "2.1.0"
 __athena_driver_version__ = "2.0.13"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
