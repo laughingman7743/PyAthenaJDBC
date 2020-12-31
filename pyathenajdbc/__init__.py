e0bcdff874bf26ac816f31eca29ff35dba023550
laughingman7743 <laughingman7743@gmail.com>
2020-12-31 16:59:33 +0900

    Add type hints

--- pyathenajdbc/__init__.py
+++ pyathenajdbc/__init__.py
@@ -1,80 +1,88 @@
 # -*- coding: utf-8 -*-
 import datetime
+from typing import TYPE_CHECKING, FrozenSet, Type
 
 from pyathenajdbc.error import *  # noqa
 
-__version__ = "2.1.1"
-__athena_driver_version__ = "2.0.14"
+if TYPE_CHECKING:
+    from pyathenajdbc.connection import Connection
+
+__version__: str = "2.1.1"
+__athena_driver_version__: str = "2.0.14"
 
 # Globals https://www.python.org/dev/peps/pep-0249/#globals
-apilevel = "2.0"
-threadsafety = 3
-paramstyle = "pyformat"
+apilevel: str = "2.0"
+threadsafety: int = 3
+paramstyle: str = "pyformat"
 
-ATHENA_JAR = "AthenaJDBC42_{0}.jar".format(__athena_driver_version__)
-ATHENA_DRIVER_DOWNLOAD_URL = (
+ATHENA_JAR: str = "AthenaJDBC42_{0}.jar".format(__athena_driver_version__)
+ATHENA_DRIVER_DOWNLOAD_URL: str = (
     "https://s3.amazonaws.com/athena-downloads/drivers/JDBC/"
     + "SimbaAthenaJDBC_{0}/{1}".format(__athena_driver_version__, ATHENA_JAR)
 )
-ATHENA_DRIVER_CLASS_NAME = "com.simba.athena.jdbc.Driver"
-ATHENA_CONNECTION_STRING = "jdbc:awsathena://AwsRegion={region};"
-LOG4J_PROPERTIES = "log4j.properties"
+ATHENA_DRIVER_CLASS_NAME: str = "com.simba.athena.jdbc.Driver"
+ATHENA_CONNECTION_STRING: str = "jdbc:awsathena://AwsRegion={region};"
+LOG4J_PROPERTIES: str = "log4j.properties"
 
 
-class DBAPITypeObject:
+class DBAPITypeObject(FrozenSet[str]):
     """Type Objects and Constructors
 
     https://www.python.org/dev/peps/pep-0249/#type-objects-and-constructors
     """
 
-    def __init__(self, *values):
-        self.values = values
-
-    def __cmp__(self, other):
-        if other in self.values:
-            return 0
-        if other < self.values:
-            return 1
+    def __eq__(self, other: object):
+        if isinstance(other, frozenset):
+            return frozenset.__eq__(self, other)
         else:
-            return -1
+            return other in self
 
-    def __eq__(self, other):
-        return other in self.values
+    def __ne__(self, other: object):
+        if isinstance(other, frozenset):
+            return frozenset.__ne__(self, other)
+        else:
+            return other not in self
+
+    def __hash__(self):
+        return frozenset.__hash__(self)
 
 
-STRING = DBAPITypeObject(
-    "CHAR",
-    "NCHAR",
-    "VARCHAR",
-    "NVARCHAR",
-    "LONGVARCHAR",
-    "LONGNVARCHAR",
-    "ARRAY",
-    "JAVA_OBJECT",
+STRING: DBAPITypeObject = DBAPITypeObject(
+    (
+        "CHAR",
+        "NCHAR",
+        "VARCHAR",
+        "NVARCHAR",
+        "LONGVARCHAR",
+        "LONGNVARCHAR",
+        "ARRAY",
+        "JAVA_OBJECT",
+    )
 )
-BINARY = DBAPITypeObject("BINARY", "VARBINARY", "LONGVARBINARY")
-BOOLEAN = DBAPITypeObject("BOOLEAN")
-NUMBER = DBAPITypeObject(
-    "TINYINT",
-    "SMALLINT",
-    "BIGINT",
-    "INTEGER",
-    "REAL",
-    "DOUBLE",
-    "FLOAT",
-    "DECIMAL",
-    "NUMERIC",
+BINARY: DBAPITypeObject = DBAPITypeObject(("BINARY", "VARBINARY", "LONGVARBINARY"))
+BOOLEAN: DBAPITypeObject = DBAPITypeObject(("BOOLEAN",))
+NUMBER: DBAPITypeObject = DBAPITypeObject(
+    (
+        "TINYINT",
+        "SMALLINT",
+        "BIGINT",
+        "INTEGER",
+        "REAL",
+        "DOUBLE",
+        "FLOAT",
+        "DECIMAL",
+        "NUMERIC",
+    )
 )
-DATE = DBAPITypeObject("DATE")
-DATETIME = DBAPITypeObject("TIMESTAMP")
+DATE: DBAPITypeObject = DBAPITypeObject(("DATE",))
+DATETIME: DBAPITypeObject = DBAPITypeObject(("TIMESTAMP",))
+
+Date: Type[datetime.date] = datetime.date
+Time: Type[datetime.time] = datetime.time
+Timestamp: Type[datetime.datetime] = datetime.datetime
 
 
-Date = datetime.date
-Time = datetime.time
-Timestamp = datetime.datetime
-
-
-def connect(*args, **kwargs):
+def connect(*args, **kwargs) -> "Connection":
     from pyathenajdbc.connection import Connection
 
     return Connection(*args, **kwargs)
--- pyathenajdbc/converter.py
+++ pyathenajdbc/converter.py
@@ -1,105 +1,23 @@
 # -*- coding: utf-8 -*-
 import binascii
 import logging
-from datetime import datetime
+from abc import ABCMeta, abstractmethod
+from copy import deepcopy
+from datetime import date, datetime
 from decimal import Decimal
+from typing import Any, Callable, Dict, Optional
 
 import jpype
 
-_logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)  # type: ignore
 
 
-def _to_none(result_set, index):
-    return None
-
-
-def _to_string(result_set, index):
-    val = result_set.getString(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    else:
-        return val
-
-
-def _to_date(result_set, index):
-    val = result_set.getDate(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return datetime.strptime(val.toString(), "%Y-%m-%d").date()
-
-
-def _to_datetime(result_set, index):
-    val = result_set.getTimestamp(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return datetime.strptime(val.toString(), "%Y-%m-%d %H:%M:%S.%f")
-
-
-def _to_float(result_set, index):
-    val = result_set.getDouble(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return float(val)
-
-
-def _to_int(result_set, index):
-    val = result_set.getLong(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return int(val)
-
-
-def _to_decimal(result_set, index):
-    val = result_set.getString(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return Decimal(val)
-
-
-def _to_boolean(result_set, index):
-    val = result_set.getBoolean(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    elif val:
-        return True
-    else:
-        return False
-
-
-def _to_array_str(result_set, index):
-    val = result_set.getArray(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return val.toString()
-
-
-def _to_binary(result_set, index):
-    val = result_set.getString(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    return binascii.a2b_hex("".join(val.split(" ")))
-
-
-def _to_default(result_set, index):
-    val = result_set.getObject(index)
-    was_null = result_set.wasNull()
-    if was_null:
-        return None
-    else:
-        return val
-
-
-class JDBCTypeConverter(object):
-    def __init__(self):
+class JDBCTypeConverter(object, metaclass=ABCMeta):
+    def __init__(
+        self,
+        mappings: Dict[str, Callable[[Any, int], Optional[Any]]],
+        default: Callable[[Any, int], Optional[Any]] = None,
+    ) -> None:
         modifier = jpype.java.lang.reflect.Modifier
         types = jpype.java.sql.Types
         self._jdbc_type_name_mappings = dict()
@@ -111,33 +29,138 @@ class JDBCTypeConverter(object):
                 self._jdbc_type_name_mappings[name] = attr
                 self._jdbc_type_code_mappings[attr] = name
         _logger.debug(self._jdbc_type_name_mappings)
-        self._converter_mappings = dict()
-        for k, v in _DEFAULT_CONVERTERS.items():
-            type_code = self._jdbc_type_name_mappings.get(k, None)
-            if type_code is not None:
-                self._converter_mappings[type_code] = v
-            else:
-                _logger.warning("%s is not defined java.sql.Types.", k)
 
-    def convert(self, type_code, result_set, index):
-        converter = self._converter_mappings.get(type_code, _to_default)
-        return converter(result_set, index)
+        self._mappings = dict()
+        if mappings:
+            for k, v in mappings.items():
+                type_code = self._jdbc_type_name_mappings.get(k, None)
+                if type_code is not None:
+                    self._mappings[type_code] = v
+                else:
+                    _logger.warning("%s is not defined java.sql.Types.", k)
+        self._default = default
 
-    def get_jdbc_type_code(self, type_name):
+    @property
+    def mappings(self) -> Dict[str, Callable[[Any, int], Optional[Any]]]:
+        return self._mappings
+
+    def get(self, type_: str) -> Optional[Callable[[Any, int], Optional[Any]]]:
+        return self.mappings.get(type_, self._default)
+
+    def set(self, type_: str, converter: Callable[[Any, int], Optional[Any]]) -> None:
+        type_code = self._jdbc_type_name_mappings.get(type_, None)
+        if type_code:
+            self._mappings[type_code] = converter
+        else:
+            _logger.warning("%s is not defined java.sql.Types.", type_)
+
+    def remove(self, type_: str) -> None:
+        self.mappings.pop(type_, None)
+
+    def update(self, mappings: Dict[str, Callable[[Any, int], Optional[Any]]]) -> None:
+        self.mappings.update(mappings)
+
+    @abstractmethod
+    def convert(self, type_code: Any, result_set: Any, index: int) -> Optional[Any]:
+        raise NotImplementedError  # pragma: no cover
+
+    def get_jdbc_type_code(self, type_name: Any) -> Any:
         return self._jdbc_type_name_mappings.get(type_name, None)
 
-    def get_jdbc_type_name(self, type_code):
+    def get_jdbc_type_name(self, type_code: Any) -> Any:
         return self._jdbc_type_code_mappings.get(type_code, None)
 
-    def register_converter(self, type_name, converter):
-        type_code = self._jdbc_type_name_mappings.get(type_name, None)
-        if type_code:
-            self._converter_mappings[type_code] = converter
-        else:
-            _logger.warning("%s is not defined java.sql.Types.", type_name)
+
+def _to_none(result_set: Any, index: int) -> None:
+    return None
 
 
-_DEFAULT_CONVERTERS = {
+def _to_string(result_set: Any, index: int) -> Optional[str]:
+    val = result_set.getString(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    else:
+        return str(val)
+
+
+def _to_date(result_set: Any, index: int) -> Optional[date]:
+    val = result_set.getDate(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return datetime.strptime(val.toString(), "%Y-%m-%d").date()
+
+
+def _to_datetime(result_set: Any, index: int) -> Optional[datetime]:
+    val = result_set.getTimestamp(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return datetime.strptime(val.toString(), "%Y-%m-%d %H:%M:%S.%f")
+
+
+def _to_float(result_set: Any, index: int) -> Optional[float]:
+    val = result_set.getDouble(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return float(val)
+
+
+def _to_int(result_set: Any, index: int) -> Optional[int]:
+    val = result_set.getLong(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return int(val)
+
+
+def _to_decimal(result_set: Any, index: int) -> Optional[Decimal]:
+    val = result_set.getString(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return Decimal(val)
+
+
+def _to_boolean(result_set: Any, index: int) -> Optional[bool]:
+    val = result_set.getBoolean(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    elif val:
+        return True
+    else:
+        return False
+
+
+def _to_array_str(result_set: Any, index: int) -> Optional[str]:
+    val = result_set.getArray(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return str(val.toString())
+
+
+def _to_binary(result_set: Any, index: int) -> Optional[bytes]:
+    val = result_set.getString(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    return binascii.a2b_hex("".join(val.split(" ")))
+
+
+def _to_default(result_set: Any, index: int) -> Optional[Any]:
+    val = result_set.getObject(index)
+    was_null = result_set.wasNull()
+    if was_null:
+        return None
+    else:
+        return val
+
+
+_DEFAULT_JDBC_CONVERTERS: Dict[str, Callable[[Any, int], Optional[Any]]] = {
     "NULL": _to_none,
     "BOOLEAN": _to_boolean,
     "TINYINT": _to_int,
@@ -178,3 +201,14 @@ _DEFAULT_CONVERTERS = {
     # 'OTHER': ???,
     # 'ROWID': ???,
 }
+
+
+class DefaultJDBCTypeConverter(JDBCTypeConverter):
+    def __init__(self) -> None:
+        super(DefaultJDBCTypeConverter, self).__init__(
+            mappings=deepcopy(_DEFAULT_JDBC_CONVERTERS), default=_to_default
+        )
+
+    def convert(self, type_code: Any, result_set: Any, index: int) -> Optional[Any]:
+        converter = self._mappings.get(type_code, _to_default)
+        return converter(result_set, index)
